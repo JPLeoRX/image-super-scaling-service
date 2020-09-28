aff041dcc37230afc5ed15cb707acb6cfad22466
leo <leo.ertuna@gmail.com>
2020-09-29 00:33:48 +0300

    Perform real upscaling in resource

--- service/resource_upscale.py
+++ service/resource_upscale.py
@@ -1,11 +1,32 @@
+import io
 import base64
-
+import numpy as np
+from PIL import Image
+from ISR.models import RDN
 from message_protocol.upscale_input import UpscaleInput
 from message_protocol.upscale_output import UpscaleOutput
 
 
+MODEL = RDN(weights='noise-cancel')
+
 class ResourceUpscale:
     def main(self, upscaleInput: UpscaleInput) -> UpscaleOutput:
-        imageBytesArray = base64.b64decode(upscaleInput.imageBase64)
+        # Parse base64 string into bytes array
+        inputImageBytesArray = base64.b64decode(upscaleInput.imageBase64)
 
-        return UpscaleOutput(upscaleInput.imageBase64)
+        # Open the image
+        imagePil = Image.open(io.BytesIO(inputImageBytesArray))
+        imageNumpy = np.array(imagePil)
+
+        # Scale the image
+        scaledNumpy = MODEL.predict(imageNumpy)
+        scaledPil = Image.fromarray(scaledNumpy)
+
+        # Write scaled image as bytes array
+        outputImageBytesArrayIO = io.BytesIO()
+        scaledPil.save(outputImageBytesArrayIO, format=imagePil.format, quality=100)
+        outputImageBytesArray = outputImageBytesArrayIO.getvalue()
+
+        # Convert back to base64 string
+        outputImageBase64 = base64.b64encode(outputImageBytesArray).decode('utf-8')
+        return UpscaleOutput(outputImageBase64)
